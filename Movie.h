50e74738aa46420cb52868012cd5aacb1a7e434d
azizmousa <azizmousa1010@gmail.com>
2020-12-07 22:19:37 +0200

    add Movie header file

---
+++ Movie.h
@@ -0,0 +1,32 @@
+#ifndef _MOVIE_H
+#define _MOVIE_H
+
+#include <string>
+
+enum Rate : int{G, PG, PG_13, R};
+
+class Movie{
+private:
+    std::string name;
+    Rate rating;
+    int watchedTimes;
+
+public:
+    //Movie constructors
+    Movie();
+    Movie(std::string name, Rate rating);
+    //movie distructor
+    ~Movie();
+
+    //getters and setters    
+    std::string getName()const{return this->name;}
+    Rate getRating()const {return this->rating;}    
+    int getWatchedTimes()const {return this->watchedTimes;}
+    void setName(std::string name){this->name = name;}
+    void setRating(Rate rating){this->rating = rating;}
+
+    // movie methods
+    void incrementWatchedTimes();
+};
+
+#endif

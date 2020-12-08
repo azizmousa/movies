9082b4fa75d831d376075871c2c9e14d5e1a491f
azizmousa <azizmousa1010@gmail.com>
2020-12-08 10:45:50 +0200

    correcting the paramitarized constructor

--- src/Movie.cpp
+++ src/Movie.cpp
@@ -1,9 +1,12 @@
 #include "movies/Movie.h"
 
+const std::string Movie::EMPTY_MOVIE{"null"};
+
 //constructor
 Movie::Movie(std::string name, Rate rating)
-    : name{"movie"}, rating{Rate::G}{
-
+    : name{EMPTY_MOVIE}, rating{Rate::NUN}{
+        this->name = name;
+        this->rating = rating;
 }
 
 //distructor

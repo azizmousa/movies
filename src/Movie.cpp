4c4f4d08f6cf58001b7208ee746b4d02ab81cbd7
azizmousa <azizmousa1010@gmail.com>
2020-12-08 13:14:48 +0200

    initialize whatching count

--- src/Movie.cpp
+++ src/Movie.cpp
@@ -4,7 +4,7 @@ const std::string Movie::EMPTY_MOVIE{"null"};
 
 //constructor
 Movie::Movie(std::string name, Rate rating)
-    : name{EMPTY_MOVIE}, rating{Rate::NUN}{
+    : name{EMPTY_MOVIE}, rating{Rate::NUN}, watchedTimes{0}{
         this->name = name;
         this->rating = rating;
 }

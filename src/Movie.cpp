742387790788fe376ad103caafe46de0c0745f1c
azizmousa <azizmousa1010@gmail.com>
2020-12-08 13:22:50 +0200

    add the watched times counter to the constructor

--- src/Movie.cpp
+++ src/Movie.cpp
@@ -3,13 +3,15 @@
 const std::string Movie::EMPTY_MOVIE{"null"};
 
 //constructor
-Movie::Movie(std::string name, Rate rating)
+Movie::Movie(std::string name, Rate rating, int watchedTimes)
     : name{EMPTY_MOVIE}, rating{Rate::NUN}, watchedTimes{0}{
         this->name = name;
         this->rating = rating;
+        this->watchedTimes = watchedTimes;
 }
 
 //distructor
 Movie::~Movie(){}
 
+//increment whatched times by 1
 void Movie::incrementWatchedTimes(){this->watchedTimes++;}

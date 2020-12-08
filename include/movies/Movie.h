7d7dbffee4b876f466d326bd599219e532edfa2d
azizmousa <azizmousa1010@gmail.com>
2020-12-08 13:32:08 +0200

    add displayer to the movie

--- include/movies/Movie.h
+++ include/movies/Movie.h
@@ -28,6 +28,7 @@ public:
 
     // movie methods
     void incrementWatchedTimes();
+    void display()const;
 };
 
 #endif
--- src/Movie.cpp
+++ src/Movie.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "movies/Movie.h"
 
 const std::string Movie::EMPTY_MOVIE{"null"};
@@ -15,3 +16,9 @@ Movie::~Movie(){}
 
 //increment whatched times by 1
 void Movie::incrementWatchedTimes(){this->watchedTimes++;}
+
+//display the movie object
+void Movie::display()const {
+    std::cout << "Mvoie Name: " <<this->getName() << "\tMovie Rate: " << this->getRating() 
+            << "\tWatched Times: " << this->getWatchedTimes() << std::endl; 
+}
